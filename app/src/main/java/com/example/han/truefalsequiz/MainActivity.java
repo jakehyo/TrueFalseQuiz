9cfb8bb1bd1488cd53fd10babebf87f21a036d46
jakehyo <jskh1999@gmail.com>
2017-09-19 14:02:22 -0700

    Korean Support, Quiz working with 4 questions

--- app/src/main/java/com/example/han/truefalsequiz/MainActivity.java
+++ app/src/main/java/com/example/han/truefalsequiz/MainActivity.java
@@ -5,8 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+
+import static com.example.han.truefalsequiz.R.id.button_next;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -31,6 +34,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         questionBank.add(new Question(getString(R.string.question_1), true));
         questionBank.add(new Question(getString(R.string.question_2), false));
         questionBank.add(new Question(getString(R.string.question_3), true));
+        questionBank.add(new Question(getString(R.string.question_4),true));
     }
 
     private void setListeners() {
@@ -42,7 +46,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void wireWidget() {
         trueButton = (Button) findViewById(R.id.button_true);
         falseButton = (Button) findViewById(R.id.button_false);
-        nextButton = (Button) findViewById(R.id.button_next);
+        nextButton = (Button) findViewById(button_next);
         questionText = (TextView) findViewById(R.id.text_question);
 
     }
@@ -50,12 +54,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         switch(view.getId()) {
-            case (R.id.button_next):
-                    questionText.setText(questionBank.get(i).getQuestionText());
-                    i++;
+            case (button_next):
+                    next(); //method for switching to the next question.
+
+                break;
+            case (R.id.button_true):
+                if (questionBank.get(i).checkAnswer(true))
+                {
+                    Toast.makeText(this, R.string.text_congrat, Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(this, R.string.text_fail, Toast.LENGTH_SHORT).show();
+                }
+                trueButton.setEnabled(false);
+                falseButton.setEnabled(false);
+                nextButton.setEnabled(true);
+                i++;
+
+                break;
+            case (R.id.button_false):
+                if (questionBank.get(i).checkAnswer(false))
+                {
+                    Toast.makeText(this, R.string.text_congrat, Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(this, R.string.text_fail, Toast.LENGTH_SHORT).show();
+                }
+                trueButton.setEnabled(false);
+                falseButton.setEnabled(false);
+                nextButton.setEnabled(true);
+                i++;
                 break;
         }
 
 
+    }
+
+    private void next() {
+        if (i > questionBank.size()-1)
+        {
+            questionText.setText(R.string.text_finish);
+            i = 0;
+        }
+        else
+        {
+            questionText.setText(questionBank.get(i).getQuestionText());
+            trueButton.setEnabled(true);
+            falseButton.setEnabled(true);
+            nextButton.setEnabled(false);
+        }
     }
 }
